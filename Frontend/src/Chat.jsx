f1010130b77803414b3e399b67f945f531d90d47
Anchal Singh <anchalsingh19654@gmail.com>
2026-08-22 21:29:20 +0530

    Updated Frontend

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -4,12 +4,17 @@ import { useContext } from 'react';
 import { MyContext } from './MyContext';
 
 function Chat() {
-  const {newChat} = useContext(MyContext);
+  const {newChat,prevChats } = useContext(MyContext);
   return (
     <>
       {newChat && <h1> Start a new Chat!</h1>}
       <div className="chats">
-
+        <div className="userDiv">
+          <p className='userMessage'> User Message</p>
+        </div>
+        <div className="gptDiv">
+          <p className='gptMessage'>GPT Generated Message</p>
+        </div>
       </div>
     
     </>
@@ -17,3 +22,13 @@ function Chat() {
 }
 
 export default Chat;
+
+
+
+
+
+
+
+
+
+
